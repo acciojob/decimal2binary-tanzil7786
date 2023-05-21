aa3bf21ba9375bb84d4eea6d6a65182cf5b67312
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-05-21 14:50:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function threeSum(arr, target) {
+function decToBin(num) {
 // write your code here
-  
+ let res="";
+	while(num>0){
+		let rem=num%2;
+		res=res+rem.toString();
+		num = Math.floor(num/2)
+	}
+	    return res.split("").reverse().join("")
 }
 
-module.exports = threeSum;
+module.exports = decToBin;
