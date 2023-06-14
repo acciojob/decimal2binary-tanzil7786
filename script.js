82a17e6452bba6305d2e9d9d9dccedff9064d0b0
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-14 21:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function decimalToBinary(dec){
+	id(dec===0){
+		return '0';
+	}
 	let binary='';
 	while(dec>0){
-		let rem=dec%2;
-		binary=rem+binary;
+		binary=(dec%2) + binary;
 		dec=Math.floor(dec/2);
 	}
 	return binary;
